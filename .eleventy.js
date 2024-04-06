58d1c4b782041922eb36c0f2b3bfc2e108263b06
Zodi Chalat <zodichalat@gmail.com>
2024-04-06 14:19:58 -0700

    post content

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,11 @@ module.exports = function(eleventyConfig){
 
     eleventyConfig.addShortcode(
       "experience",
-      (place, role, description) =>
+      (year, place, role, description) =>
       `<div>
       <div class="flex justify-between cursor-pointer" onclick="toggleExpand(this)">
           <div class="flex justify-between w-4/12">
-              <p>2023</p>
+              <p>${year}</p>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
                 </svg>
