04645fc2bedab0209b3a943a22e84431f4554b39
Zodi Chalat <zodichalat@gmail.com>
2024-04-08 21:08:20 -0700

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
     eleventyConfig.addPassthroughCopy("src/assets/js/script.js");
+    eleventyConfig.addPassthroughCopy("src/assets/imgs/favicon.ico");
     eleventyConfig.addPlugin(require("eleventy-plugin-heroicons"));
 
     eleventyConfig.addShortcode(
